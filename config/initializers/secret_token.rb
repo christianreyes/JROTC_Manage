3a576c649e20109eb418f74f5f5a3d056a7a3b26
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-04-03 01:39:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JROTCManage::Application.config.secret_token = 'c1c8d6abbb9f78f02666b5d7de889dc3ee87153120f497f32603815eca2d5fe99802d1f2d5cc0625c63841b0e5451fda298f02806287e6cec8ba48b8c955002f'
