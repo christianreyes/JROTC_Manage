02b4ef0a3c8a3392829f4e7827f676befebfb235
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-04-07 14:22:13 -0400

    improving feedback email header

--- app/mailers/feedback.rb
+++ app/mailers/feedback.rb
@@ -3,7 +3,8 @@ class Feedback < ActionMailer::Base
   
   def feedback_message(feedback)
   @feedback = feedback
-   	mail(:from => @feedback[:email], 
+   	mail(:from => "\"JROTC Manage Feedback\"<jrotcmange@gmail.com>",
+		 :reply_to => @feedback[:email]
 	     :to => "jrotcmanage@gmail.com",
 		 :subject => "AUTO: Feedback from website")
   end
