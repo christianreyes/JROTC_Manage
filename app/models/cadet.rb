a463410a5ec45e784f3a3c82ace36bd941bd138c
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-04-09 04:09:13 -0400

    ordering by lastname, firstname asc

--- app/models/cadet.rb
+++ app/models/cadet.rb
@@ -2,8 +2,10 @@ class Cadet < ActiveRecord::Base
 	has_many :awards, :dependent => :destroy
 	has_many :ribbons, :through => :awards
 	belongs_to :ribbon
-
+	
+	scope :all, order(:last_name.asc, :first_name.asc)
+	
 	def name
-		first_name + " " + last_name
+		last_name + ", " + first_name
 	end
 end
