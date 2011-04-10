94921b36aed9bf3fe580d26683f6d92e75b3ecf0
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-04-10 17:23:12 -0400

    another method of getting first initial

--- app/models/cadet.rb
+++ app/models/cadet.rb
@@ -8,7 +8,7 @@ class Cadet < ActiveRecord::Base
 	
 	def name
 		if middle_name
-			last_name + ", " + first_name + " " + middle_name[0].to_s
+			last_name + ", " + first_name + " " + middle_name.chars.to_a[0]
 		else
 			last_name + ", " + first_name
 		end
