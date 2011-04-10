b44b3f085d5ab44c27263c2e5663e92455850508
George Sopp <gdsopp@gmail.com>
2011-04-09 21:43:42 -0700

    created longitude in event

---
+++ db/migrate/20110410044232_add_longitude_to_event.rb
@@ -0,0 +1,9 @@
+class AddLongitudeToEvent < ActiveRecord::Migration
+  def self.up
+    add_column :events, :longitude, :float
+  end
+
+  def self.down
+    remove_column :events, :longitude
+  end
+end
