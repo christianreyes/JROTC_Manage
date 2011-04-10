b1aa470f26f6cc44ca8b7e1a70f054751c46dc38
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-04-10 01:10:21 -0400

    scopes added

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,10 @@
 class Event < ActiveRecord::Base
 	belongs_to :cadet, :foreign_key => "cadet_in_charge_id"
 	
+	scope :all, order(:name.asc)
+	scope :upcoming, where(:date > Date.yesterday).order(:date.desc)
+    scope :past, where(:date <= Date.yesterday).order(:date.desc)
+	
 	validates :name, :presence => true
 	validates :date, :presence => true
 end
